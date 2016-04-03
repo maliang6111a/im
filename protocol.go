02ddf4b32e063b779f0d02e5698cf6f41a1c6e6d
maliang <449047622@qq.com>
2016-04-03 20:58:58 +0800

    删除header_protocol.go

--- protocol.go
+++ protocol.go
@@ -28,6 +28,33 @@ type IMessage interface {
 	FromData(buff []byte) bool
 }
 
+//消息
+type Message struct {
+	version  int         //1,byte,消息协议版本
+	msg_type int         //1,byte,消息类型
+	body     interface{} //len
+}
+
+func (this *Message) ToData() []byte {
+	if this.body != nil {
+		if m, ok := this.body.(IMessage); ok {
+			return m.ToData()
+		}
+	}
+	return make([]byte, 0)
+}
+
+func (this *Message) FromData(buff []byte) bool {
+	proVersion := this.version
+	if creator, ok := message_creators[proVersion]; ok {
+		c := creator()
+		r := c.FromData(buff)
+		this.body = c
+		return r
+	}
+	return len(buff) == 0
+}
+
 //写入消息头
 //version  1
 //msg_type 1
